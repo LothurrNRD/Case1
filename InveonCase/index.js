0d0e85300dff82861a4d692cb3d4d695779c2100
LothurrNRD <98153522+LothurrNRD@users.noreply.github.com>
2023-03-09 03:40:04 +0300

    Last

--- InveonCase/index.js
+++ InveonCase/index.js
@@ -109,7 +109,7 @@ function items() {
   }
   $(".sec2").append(items);
 }
-function createArray() {
+function createSelector() {
   for (var i = 0; i < 4; i++) {
     if (i <= 2) {
       $(".third").append("<div></div>");
